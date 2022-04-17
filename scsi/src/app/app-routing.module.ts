bb2c55ce7362cf0b267939288dd5287cbc77ec3b
FahadHossain9 <bsse1105@iit.du.ac.bd>
2022-04-17 10:31:59 +0600

    add routing

--- scsi/src/app/app-routing.module.ts
+++ scsi/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NetPackageListComponent } from './net-package-list/net-package-list.component';
+import { OperatorNavbarComponent } from './operator-navbar/operator-navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: NetPackageListComponent },
+  { path: 'operatorHome', component: OperatorNavbarComponent },
+  {path:'sysAdminHome',component:SysAdminNavbarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
