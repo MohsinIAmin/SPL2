bf289e09ea97829c45ccd3fd58e39d17850216f7
MohsinIAmin <bsse1101@iit.du.ac.bd>
2022-04-19 09:29:12 +0600

    operator home

--- scsi/src/app/sys-admin-home/sys-admin-home.component.ts
+++ scsi/src/app/sys-admin-home/sys-admin-home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NetPackage } from '../models/net-package';
+import { PackageService } from '../services/package.service';
 
 @Component({
   selector: 'app-sys-admin-home',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SysAdminHomeComponent implements OnInit {
 
-  constructor() { }
+  allPackage: NetPackage[] = [];
+  constructor(private router: Router, private packageService: PackageService) { }
 
   ngOnInit(): void {
+    this.getAllPackage();
+  }
+
+  getAllPackage() {
+    this.packageService.getAllPackage()
+      .subscribe(response => { 
+        this.allPackage = response.map(item=>{
+          return new NetPackage(item.name,item.speed,item.cost);
+        });
+      });
   }
 
 }
