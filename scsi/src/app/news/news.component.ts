42ef4d0086e5faacd84317e2da69724498e69e07
MohsinIAmin <bsse1101@iit.du.ac.bd>
2022-06-06 17:12:39 +0600

    news

--- scsi/src/app/news/news.component.ts
+++ scsi/src/app/news/news.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { News } from '../models/news';
+import { NewsService } from '../services/news.service';
 
 @Component({
   selector: 'app-news',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+  allNews:News[] = [];
+
+  constructor(private newsService: NewsService,private router: Router) { }
 
   ngOnInit(): void {
+    this.getAllNews();
+  }
+
+  getAllNews() {
+    this.newsService.getAllNews()
+      .subscribe(response => { 
+        this.allNews = response.map(item=>{
+          return new News(item.description,item.heading,item.operator_id,item.timestamp);
+        });
+      });
   }
 
 }
